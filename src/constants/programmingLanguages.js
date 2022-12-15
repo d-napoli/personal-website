a377c343d82c6baa8e3be115aacf13b74b46b06e
Diego Napoli <d.napoli.dev@gmail.com>
2022-12-15 13:58:01 -0300

    Change default export

--- src/constants/programmingLanguages.js
+++ src/constants/programmingLanguages.js
@@ -1,6 +1,6 @@
 import { Python, SQL, HTML, React, Node, CSS } from '../constants/images'
 
-let programing_languages = [
+const programing_languages = [
     { id: 0, icon: Python, name: 'Python', beginYear: 2018 },
     { id: 1, icon: React, name: 'React', beginYear: 2019 },
     { id: 2, icon: SQL, name: 'SQL', beginYear: 2016 },
@@ -9,4 +9,4 @@ let programing_languages = [
     { id: 5, icon: CSS, name: 'CSS', beginYear: 2015 }
 ]
 
-export default { programing_languages }
+export default programing_languages
